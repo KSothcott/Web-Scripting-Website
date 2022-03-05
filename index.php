b7181d7bd60079f0297805da198c06b84673e2f1
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-05 13:20:20 +0000

    Made icon bar on index.php

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 
 <head>
-
-<title>Homepage</title>
+    <title>Homepage</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 <style>
 
     .heading {
@@ -12,12 +13,31 @@
 		text-align: center;
         font-family: verdana;
     }
+      
+	.icon-bar {
+    width: 100%;
+    background-color: #509D8A;
+    overflow: auto;
+    }
+
+    .icon-bar a {
+    float: left;
+    text-align: center;
+    padding: 12px;
+    transition: all 0.3s ease;
+    color: white;
+    font-size: 36px;
+    }   
     
     
 </style>
 </head>
 
 <body style="background-color: #F5ECEB; font-family: verdana;">
+
+    <div class="icon-bar">
+    <a href="http://localhost/Web-Scripting-Website/" style="font-size: 47px;"><i class="fa fa-home"></i></a> 
+    </div>
 
     <div class="heading">
 	<p>Homepage</p>
