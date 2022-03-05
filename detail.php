1075f6d2875690979eea416e4a7ce2488a664357
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-05 13:01:08 +0000

    Set up heading and body styles

---
+++ detail.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+
+<title>Recipe Details</title>
+<style>
+
+    .heading {
+        float: center;
+		font-size: 34px;
+		margin: 20px;
+		text-align: center;
+        font-family: verdana;
+    }
+    
+    
+</style>
+</head>
+
+<body style="background-color: #F5ECEB; font-family: verdana;">
+
+    <div class="heading">
+	<p>Recipe Details</p>
+	</div>
+
+    <p>Text</p>
+
+</body>
+
+</html>
+
---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+
+<title>Homepage</title>
+<style>
+
+    .heading {
+        float: center;
+		font-size: 34px;
+		margin: 20px;
+		text-align: center;
+        font-family: verdana;
+    }
+    
+    
+</style>
+</head>
+
+<body style="background-color: #F5ECEB; font-family: verdana;">
+
+    <div class="heading">
+	<p>Homepage</p>
+	</div>
+
+    <p>Text</p>
+
+</body>
+
+</html>
+
