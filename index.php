bf3a4fd0d462b7fbe3d977909ed0c7ad7370d6f2
KSothcott <80410150+KSothcott@users.noreply.github.com>
2022-03-12 20:26:42 +0000

    Improved page layout
    Fixed table alignment, moved logo

--- index.php
+++ index.php
@@ -49,18 +49,12 @@
     background-color: #747875;
     }    
     
-    .image {
-        height: 200px;
-        width: 200px;
-        padding: 50px;
+    .table {
+        width: 100%;
+        padding-left: 50px;
         
     }
 
-    .name {
-        font-family: verdana;
-        float: center;
-        font-size: 18px;
-        
 
     }
     
@@ -73,15 +67,15 @@
     <div class="icon-bar">
         <a href="http://localhost/Web-Scripting-Website/" style="font-size: 47px;"><i class="fa fa-home"></i></a> 
     </div>
-
+       
     <div class="heading">
 	    <p>Recipes for You</p>
-	</div>
-
+     	</div>
+     
     <img src="Logo.png" style="float: right;width: 200px;height: 200px;padding-right: 50px;"/>
-
+    
     <p style="padding-left: 50px;">Welcome to Recipes for You. <br /> <br /> Please choose from our selection of recipes below for more details.</p>
-    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <br /><br /><br />
 
     
 <?php
@@ -89,44 +83,30 @@
     
     $query = ' SELECT * FROM `recipes` ';
 
-    //$result = $dbc->query($query);
     $result = mysqli_query($dbc,$query);
     
     if ($result)
     {   
-        echo '<table style="width: 100%;"><tr>';
-        $sounter=0;
+        echo '<table class="table"><tr>';
+        $counter=0;
         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
         {
             $image_address = '<img src="'.$row['Image'].'" width="200" height="0200" />';
             $recipe_name = $row['RecipeName'];
             $price = '&pound;'.$row['Price']/100;
-            If($counter++ == 2 )  { echo '</tr><tr>'; $counter=0;}                      
-      //      echo '<a href = "detail.php"><img src="'.$image_address.'" class="image" />';
-        //    echo $recipe_name;
-      //      echo " ";
-        //    echo $price;
+            If($counter >=3)
+                { echo '</tr><tr>'; $counter=1;}     
+                else $counter++;                 
+
         
         echo '<td>'.$image_address.'<br />'.$recipe_name.'<br />'.$price.'</td>';
         }
         echo '</tr></table>';
     }
 
-    //$row = $result->fetch_assoc();
-
     $image_address = $row['Image'];
     $recipe_name = $row['RecipeName'];
     $price = $row['Price'];
-
-    //echo '<img src="'.$image_address.'" class="image"/>'
-    
-    
-    
-    //<tr>
-        //<td align="left" width="33%">' SELECT `Image` FROM `recipes` WHERE  `RecipeID` = "2"'</td>
-        //<td align="center" width="33%">#</td>
-        //<td align="right" width="33%">#</td>
-    //</tr>';
 
 
 ?>
